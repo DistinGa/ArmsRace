f911cd7fb7a92e909fc7d7f03167f4f3cf0168ae
Дмитрий Вавилов <dvav@mail.ru>
2017-06-29 01:45:18 +0700

    Изменение флагов в нижней панели в зависимости от режима в стране.

---
+++ Assets/Scripts/BottomFlagPanelScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomFlagPanelScript : MonoBehaviour {
+    [SerializeField]
+    CountryScript country;
+    [SerializeField]
+    Image fImage;
+    [SerializeField]
+    Sprite USAFlag, USSRFlag;
+
+    void Update () {
+        if (fImage != null)
+        {
+            if (country.Authority == Authority.Soviet)
+                fImage.sprite = USSRFlag;
+            else
+                fImage.sprite = USAFlag;
+        }
+    }
+}
